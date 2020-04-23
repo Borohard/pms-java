2340fb571689e28a05cd5fbd1180a5e600a9b010
Borohard <43915070+Borohard@users.noreply.github.com>
2020-04-23 17:25:20 +0500

    Delete DepartmentMapper.java

--- src/main/java/pms/api/mappers/DepartmentMapper.java
+++
@@ -1,9 +0,0 @@
-package pms.api.mappers;
-
-import pms.api.dto.DepartmentDto;
-import pms.api.models.Department;
-
-public interface DepartmentMapper {
-    DepartmentDto ToDtoFromDepartment(Department department);
-    Department ToDepartmentFromDto(DepartmentDto departmentDto);
-}
