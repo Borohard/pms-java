2c189b13ab925cb969a6baa7e23f42f39495db0f
Borohard <43915070+Borohard@users.noreply.github.com>
2020-04-23 17:25:27 +0500

    Delete UserMapper.java

--- src/main/java/pms/api/mappers/UserMapper.java
+++
@@ -1,9 +0,0 @@
-package pms.api.mappers;
-
-import pms.api.dto.UserRegisterDto;
-import pms.api.models.User;
-
-public interface UserMapper {
-    User ToUserFromRegisterDto(UserRegisterDto registerDto);
-
-}
