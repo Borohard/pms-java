6e9be9f395704292d9fe793ad4a1bb3aaf3b5532
Borohard <43915070+Borohard@users.noreply.github.com>
2020-04-23 17:26:10 +0500

    Delete ClaimPositionsRepository.java

--- src/main/java/pms/api/repositories/ClaimPositionsRepository.java
+++
@@ -1,9 +0,0 @@
-package pms.api.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RestResource;
-import pms.api.models.ClaimPosition;
-
-@RestResource(exported = false)
-public interface ClaimPositionsRepository extends JpaRepository<ClaimPosition, Long> {
-}
